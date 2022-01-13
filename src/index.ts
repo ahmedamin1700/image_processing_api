c52f4de3f8074e214e05d2db626f8c70bbdec150
Ahmed Amin <ahmedamin1700@gmail.com>
2022-01-13 03:08:35 +0200

    Create and configure express server.
    - add '/api/images' and working.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-const myFunc = (num: number): number => {
-  return num * num;
-};
+import express from "express";
 
-export default myFunc;
+const app = express();
+const port = 3000;
+
+app.get("/api/images", (req, res) => {
+  res.send("working");
+});
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
