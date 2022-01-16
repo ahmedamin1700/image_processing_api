df49150fd34399ed79d6f57d53b9b0d120985b0b
Ahmed Amin <ahmedamin1700@gmail.com>
2022-01-16 14:01:15 +0200

    Create images router
    - app using router for '/api/images'
    - removed serving static files for 'public' folder
     as need to use the same link to process the image as well as serving the image file.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import express from "express";
+import imagesRoute from "./routes/api/images";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
-
-app.get("/api/images", (req, res) => {
-  res.send("working");
-});
+app.use("/api", imagesRoute);
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
